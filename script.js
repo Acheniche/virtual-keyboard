49b3bb4c6151b93cac5cf94f4d245f88a3ce4c41
Acheniche <ache.niche00@mail.ru>
2023-04-27 23:00:09 +0300

    feat: add keyboard keys

---
+++ script.js
@@ -0,0 +1,36 @@
+let numberKeys = [ 
+    "Backquote","Digit1","Digit2","Digit3","Digit4","Digit5","Digit6","Digit7","Digit8","Digit9","Digit0","Minus","Equal","Backspace",
+    "Tab","KeyQ","KeyW","KeyE","KeyR","KeyT","KeyY","KeyU","KeyI","KeyO","KeyP","BracketLeft","BracketRight","Backslash","Delete",
+    "CapsLock","KeyA","KeyS","KeyD","KeyF","KeyG","KeyH","KeyJ","KeyK","KeyL","Semicolon","Quote","Enter",
+    "ShiftLeft","KeyZ","KeyX","KeyC","KeyV","KeyB","KeyN","KeyM","Comma","Period","Slash","ArrowUp","ShiftRight",
+    "ControlLeft","MetaLeft","AltLeft","Space","AltRight","ControlRight","ArrowLeft","ArrowDown","ArrowRight"];
+
+const enKeys = [
+    '`','1','2','3','4','5','6','7','8','9','0','-','=','Backspace',
+    'Tab','q','w','e','r','t','y','u','i','o','p','[',']','\\','Del',
+    'Caps Lock','a','s','d','f','g','h','j','k','l',';','\'','Enter',
+    'LShift','z','x','c','v','b','n','m',',','.','/','↑','RShift',
+    'LCtrl','Win','LAlt','Space','RAlt','RCtrl','←','↓','→'];
+
+const ruKeys = [
+    'ё','1','2','3','4','5','6','7','8','9','0','-','=','Backspace',
+    'Tab','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','\\','Del',
+    'Caps Lock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter',
+    'LShift','я','ч','с','м','и','т','ь','б','ю','.','↑','RShift',
+    'LCtrl','Win','LAlt','Space','RAlt','RCtrl','←','↓','→'];
+
+const enKeysCaps = [
+    '~','!','@','#','$','%','^','&','*','(',')','_','+','Backspace',
+    'Tab','Q','W','E','R','T','Y','U','I','O','P','{','}','|','Del',
+    'Caps Lock','A','S','D','F','G','H','J','K','L',':','"','Enter',
+    'LShift','Z','X','C','V','B','N','M','<','>','?','↑','RShift',
+    'LCtrl','Win','LAlt','Space','RAlt','RCtrl','←','↓','→'];
+
+const ruKeysCaps = [
+    'Ё','!','"','№',';','%',':','?','*','(',')','_','+','Backspace',
+    'Tab','Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','/','Del',
+    'Caps Lock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter',
+    'LShift','Я','Ч','С','М','И','Т','Ь','Б','Ю',',','↑','RShift',
+    'LCtrl','Win','LAlt','Space','RAlt','RCtrl','←','↓','→'];
+
+      
